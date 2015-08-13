706f68f5133e4a35fe49ce18abe57efb5abc7fe1
Elissa Tong <elissa.tong@gmail.com>
2015-08-12 21:56:15 -0700

    Initial project with generic modal for 1-3 buttons.

---
+++ GenericModalWindow/Assets/Scripts/ModalPanel.cs
@@ -0,0 +1,95 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using System.Collections;
+
+public class ModalPanel : MonoBehaviour {
+
+	public Text questions;
+	public Image icon;
+	public Button [] buttons;
+	public GameObject modalPanel;
+
+	private static ModalPanel sModalPanel;
+
+	public static ModalPanel Instance() {
+		if (!sModalPanel) {
+			sModalPanel = FindObjectOfType(typeof (ModalPanel)) as ModalPanel;
+
+			if (!sModalPanel) {
+				Debug.LogError("There needs to be at least one active ModalPanel script on a GameObject in your scene.");
+			}
+		}
+
+		return sModalPanel;
+	}
+
+	// Implement a Yes/No/Cancel panel
+	public void YesNoCancelModal(string question, UnityAction yesEvent, UnityAction noEvent, UnityAction cancelEvent) {
+
+		modalPanel.SetActive(true);
+
+		if (buttons.Length == 3)
+		{
+			buttons[0].onClick.RemoveAllListeners();
+			buttons[0].onClick.AddListener(yesEvent);
+			buttons[0].onClick.AddListener(ClosePanel);
+
+			buttons[1].onClick.RemoveAllListeners();
+			buttons[1].onClick.AddListener(noEvent);
+			buttons[1].onClick.AddListener(ClosePanel);
+
+			buttons[2].onClick.RemoveAllListeners();
+			buttons[2].onClick.AddListener(cancelEvent);
+			buttons[2].onClick.AddListener(ClosePanel);
+
+			this.questions.text = question;
+			this.icon.gameObject.SetActive(false);
+			this.buttons[0].gameObject.SetActive(true);
+			this.buttons[1].gameObject.SetActive(true);
+			this.buttons[2].gameObject.SetActive(true);
+		}
+	}
+
+	public void EnableModal(string question, UnityAction [] events, Sprite icon = null) {
+
+		modalPanel.SetActive(true);
+
+		this.questions.text = question;
+
+		if (icon != null) 
+		{
+			this.icon.gameObject.SetActive(true);
+			this.icon.sprite = icon;
+		}
+
+		// Check if there is enough buttons for the requested events
+		int numButtons = buttons.Length;
+
+		for (int i = 0; i < events.Length; ++i) {
+
+			if (i < numButtons) {
+				buttons[i].onClick.RemoveAllListeners();
+				buttons[i].onClick.AddListener(events[i]);
+				buttons[i].onClick.AddListener(ClosePanel);
+				buttons[i].gameObject.SetActive(true);
+
+				Text gtext = buttons[i].GetComponentInChildren<Text>();
+				gtext.text = "Hello" + i;
+			}
+
+		}
+
+		// Deactivate any unused buttons
+		for (int i = events.Length; i < buttons.Length; ++i)
+		{
+			buttons[i].gameObject.SetActive(false);
+		}
+
+	}
+
+	void ClosePanel() {
+		modalPanel.SetActive(false);
+	}
+
+}
---
+++ GenericModalWindow/Assets/Scripts/TestModalPanel.cs
@@ -0,0 +1,86 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using System.Collections;
+
+
+public class TestModalPanel : MonoBehaviour {
+
+	public GameObject background;
+	public Texture [] textures;
+	public Texture2D icon;
+	public Text coins;
+
+	private ModalPanel modalPanel;
+	private UnityAction myYesAction;
+	private UnityAction myNoAction;
+	private UnityAction myCancelAction;
+	private UnityAction myOkAction;
+
+	private Rect SPRITE_RECT = new Rect(0.0f, 0.0f, 32.0f, 320.0f);
+	private Vector2 SPRITE_PIVOT = new Vector2(0.5f, 0.5f);
+	private Sprite sIcon;
+
+	private int currentTexture = 0;
+	private int numCoins = 0;
+
+	void Awake() {
+		modalPanel = ModalPanel.Instance();
+		myYesAction = new UnityAction(TestYesFunction);
+		myNoAction = new UnityAction(TestNoFunction);
+		myCancelAction = new UnityAction(TestCancelFunction);
+		myOkAction = new UnityAction(TestOkFunction);
+	}
+
+	public void TestYesNoCancel() {
+		modalPanel.YesNoCancelModal("Do you want to change background?", myYesAction, myNoAction, myCancelAction);
+	}
+
+	public void Test3Buttons() {
+		UnityAction [] actions = {myYesAction, myNoAction, myCancelAction};
+
+		Sprite sIcon = Sprite.Create(icon, SPRITE_RECT, SPRITE_PIVOT);
+		modalPanel.EnableModal("Do you want to change background?", actions, sIcon);
+	}
+
+	public void Test2Buttons() {
+		UnityAction [] actions = {myYesAction, myNoAction};
+		modalPanel.EnableModal("Do you want to change background?", actions);
+	}
+
+	public void Test1Buttons() {
+		UnityAction [] actions = {myOkAction};
+		Sprite sIcon = Sprite.Create(icon, SPRITE_RECT, SPRITE_PIVOT);
+		modalPanel.EnableModal("You have been rewarded 100 coins.", actions, sIcon);
+	}
+
+	void TestYesFunction() {
+		//Debug.Log("TestYesFunction");
+		MeshRenderer meshRenderer = background.GetComponent<MeshRenderer>();
+		if (meshRenderer != null) {
+
+			currentTexture = (++currentTexture) % textures.Length;
+
+			meshRenderer.materials[0].mainTexture = textures[currentTexture];
+		}
+	}
+
+	void TestNoFunction() {
+		// add logic
+	}
+
+	void TestCancelFunction() {
+		// add logic
+	}
+
+	void TestOkFunction() {
+
+		//Debug.Assert (coins != null, "Text Coins can't be null. Assign a GameObject.");
+
+		//if (coins != null) 
+		{
+			numCoins += 100;
+			coins.text = "100 coins";
+		}
+	}
+}
